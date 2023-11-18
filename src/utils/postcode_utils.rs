8b33126f43692c3b16803bbd947ee4ec9e01d713
Marco Haucke <marco.haucke@tum.de>
2023-11-18 22:31:25 +0100

    utils update

--- src/utils/postcode_utils.rs
+++ src/utils/postcode_utils.rs
@@ -1,11 +1,16 @@
+use axum::routing::post;
+use geoutils::Location;
+use sea_orm::ActiveValue;
+
 use crate::database::sea_orm_active_enums::InGroup;
 use crate::database::{filtered_ranks, postcode};
 use crate::rest::patch_craftsmen;
 
+const DEFAULT_DISTANCE: f64 = 80.0;
+
 pub struct Postcode {
     postcode: i32,
-    lat: f64,
-    lon: f64,
+    loc: Location,
     offset: f64,
 }
 
@@ -31,8 +36,7 @@ impl Into<Postcode> for postcode::Model {
 
         Postcode {
             postcode,
-            lat,
-            lon,
+            loc: Location::new(lat, lon),
             offset: postcode_extension_distance_group.get_offset(),
         }
     }
@@ -40,19 +44,37 @@ impl Into<Postcode> for postcode::Model {
 
 // TODO renameme
 pub struct PatchFilters {
-    max_driving_distance: i64,
-    profile_score: i64,
+    profile_id: i32,
+    max_driving_distance: f64,
+    profile_score: f64,
+    loc: Location,
 }
 
 impl Postcode {
-    pub fn get_model_opt(&self, patch: PatchFilters) -> Option<filtered_ranks::ActiveModel> {
+    pub fn get_model_opt(&self, patch: &PatchFilters) -> Option<filtered_ranks::ActiveModel> {
         let Self {
             postcode,
-            lat,
-            lon,
+            loc,
             offset,
         } = self;
 
-        todo!()
+        let dist: f64 = loc.haversine_distance_to(&patch.loc).meters();
+
+        if dist > patch.max_driving_distance + offset {
+            return None;
+        }
+
+        let distance_score = 1.0 - dist / DEFAULT_DISTANCE;
+
+        let distance_weight = if dist > DEFAULT_DISTANCE { 0.01 } else { 0.15 };
+
+        let rank = distance_weight * distance_score + (1.0 - distance_weight) * patch.profile_score;
+
+        Some(filtered_ranks::ActiveModel {
+            profile_id: ActiveValue::Set(patch.profile_id),
+            postcode: ActiveValue::Set(*postcode),
+            distance: ActiveValue::Set(dist),
+            rank: ActiveValue::Set(rank),
+        })
     }
 }
